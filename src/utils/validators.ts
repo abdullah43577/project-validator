8c1632127c5d5e520d2310fed8062630c205747e
Abdullah Ayoola <officialayo540@gmail.com>
2025-03-13 23:27:33 +0100

    removed max lenght on string

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,21 +1,21 @@
 import Joi from "joi";
 
 export const projectSchema = Joi.object({
-  author_name: Joi.string().min(3).max(50).required(),
-  project_title: Joi.string().min(5).max(100).required(),
+  author_name: Joi.string().min(3).required(),
+  project_title: Joi.string().min(5).required(),
   date_of_submission: Joi.date().iso().required(),
-  abstract: Joi.string().min(10).max(500).required(),
-  aims: Joi.string().min(10).max(500).required(),
-  objectives: Joi.array().items(Joi.string().min(3).max(200)).min(1).max(20).required(),
-  supervisor: Joi.string().min(3).max(50).required(),
+  abstract: Joi.string().min(10).required(),
+  aims: Joi.string().min(10).required(),
+  objectives: Joi.array().items(Joi.string().min(3)).min(1).max(20).required(),
+  supervisor: Joi.string().min(3).required(),
 });
 
 export const editProjectSchema = Joi.object({
-  author_name: Joi.string().min(3).max(50).optional(),
-  project_title: Joi.string().min(5).max(100).optional(),
+  author_name: Joi.string().min(3).optional(),
+  project_title: Joi.string().min(5).optional(),
   date_of_submission: Joi.date().iso().optional(),
-  abstract: Joi.string().min(10).max(500).optional(),
-  aims: Joi.string().min(10).max(500).optional(),
-  objectives: Joi.array().items(Joi.string().min(3).max(200)).min(1).max(20).optional(),
-  supervisor: Joi.string().min(3).max(50).optional(),
+  abstract: Joi.string().min(10).optional(),
+  aims: Joi.string().min(10).optional(),
+  objectives: Joi.array().items(Joi.string().min(3)).min(1).max(20).optional(),
+  supervisor: Joi.string().min(3).optional(),
 });
