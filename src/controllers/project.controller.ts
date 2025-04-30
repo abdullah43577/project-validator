8001cd807ae9141635d1f87e61cc6299f6f284d9
Bilal <muilibilal@gmail.com>
2025-05-01 00:31:27 +0100

    fixes

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -166,7 +166,7 @@ const deleteProject = async function (req: Request, res: Response) {
     console.log(req.body);
 
     if (
-      project.admin_id !== req.body.admin_id ||
+      project.admin_id !== req.body.admin_id &&
       req.body.admin_id !== "PJVLD001"
     )
       return res.status(400).json({ message: "Invalid Admin ID" });
